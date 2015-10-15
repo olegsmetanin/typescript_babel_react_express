39c2008264bfff30088b5ef96315165f296bb6cd
Oleg Smetanin <soa@inbox.ru>
2015-10-15 23:49:03 +0300

    fix latency calculations

--- src/server/helpers/invoke.ts
+++ src/server/helpers/invoke.ts
@@ -6,7 +6,7 @@ export default async function invoke<T>(command: ICommand<T>) {
   var startTime = global.process.hrtime();
   var value = await command.execute();
   var diff = global.process.hrtime(startTime);
-  console.log(name+ ': '+ (diff[0] * 1000) + (diff[1] / 1000000) + 'ms')
+  console.log(name + ': ' +((diff[0] * 1000) + (diff[1] / 1000000)) + 'ms')
   return value;
 
 }
